3637a943de31ce3fd715c10fa8283d225aa55ac1
ipraveen3 <1729praveen@gmail.com>
2021-02-05 02:03:30 +0530

    fetching data from firebase and added relevant redux type, action,reducer

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -49,6 +49,25 @@ const config = {
      return await batch.commit();
   }
 
+export  const convertCollectionSnapshotToMap = (collections) => {
+      const transformedCollection = collections.docs.map(
+          doc => {
+              const{ title, items } = doc.data();
+              return {
+                  routeName : encodeURI(title.toLowerCase()),
+                  id: doc.id,
+                  title,
+                  items 
+              }
+          }
+      )
+    
+    return transformedCollection.reduce((accumulator, collection) => {
+          accumulator[collection.title.toLowerCase()] = collection;
+          return accumulator;  
+      },{});
+  };
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
